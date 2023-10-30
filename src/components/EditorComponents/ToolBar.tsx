8d129b02d55c3c2769bd00eecefb09aa1e4b9ae8
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2023-10-29 20:53:13 -0500

    update toolbar

--- src/components/EditorComponents/ToolBar.tsx
+++ src/components/EditorComponents/ToolBar.tsx
@@ -5,10 +5,30 @@ import { MdPublish, MdShare, MdOutlineSave } from "react-icons/md";
 interface Props {}
 
 const MyComponent: React.FC<Props> = () => {
+  const [editName, setEditName] = React.useState(false);
+  const [fileName, setFileName] = React.useState("test.py");
+
+  const onClickText = () => {
+    setEditName(editName ? false : true);
+  };
+
   return (
     <div className="bg-gray-500 text-white px-1 py-[.25rem]">
       <div className="flex flex-row justify-between">
-        <p className="text-left">test.py</p>
+        <div className="w-[50%]" onClick={onClickText}>
+          {editName ? (
+            <p className="text-left">{fileName}</p>
+          ) : (
+            <p className="text-left">
+              <input
+                className="bg-transparent border-none in"
+                value={fileName}
+                onChange={(e) => setFileName(e.target.value)}
+                autoFocus
+              />
+            </p>
+          )}
+        </div>
 
         <div>
           <div className="text-right flex space-x-1">
